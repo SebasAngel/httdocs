65f1a7faa8a3e10747a9c8832422f8ab23059c25
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-04 09:02:21 -0500

    proyecto angular_api_php

--- app01/src/main.ts
+++
@@ -1,9 +0,0 @@
- import { bootstrapApplication } from '@angular/platform-browser';
- import { HousingLocationComponent } from './app/components/housing-location/housing-location.component';
-//  import { AppComponent } from './app/app.component';
- import { routes } from './app/app.routes';
- import { provideRouter } from '@angular/router';
-
- bootstrapApplication(HousingLocationComponent,{
-   providers: [provideRouter(routes)]
- }).catch((err) => console.error(err));
