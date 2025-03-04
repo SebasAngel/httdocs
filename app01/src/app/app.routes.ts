fdb080692db618b2ba816b6faaf2fd10979965b7
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-04 12:10:11 -0500

    proyecto angular_api_php

--- app01/src/app/app.routes.ts
+++ app01/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HousingLocationComponent } from './components/housing-location/housing-location.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'locations', component: HousingLocationComponent },
+    { path: '', redirectTo: '/locations', pathMatch: 'full' }
+];
--- app01/src/app/services/housing-location.service.ts
+++ app01/src/app/services/housing-location.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HousingLocation } from '../interfaces/housing-location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingLocationService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/locations';
+
+  constructor(private http: HttpClient) { }
+
+  getAllLocations(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
