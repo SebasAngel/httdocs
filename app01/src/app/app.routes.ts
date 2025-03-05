9af9901ed60c101defc48a20c7f2595ca6510553
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-03-04 21:18:00 -0500

    01. Creación de la APIREST en PHP

--- app01/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HousingLocationComponent } from './components/housing-location/housing-location.component';
-
-export const routes: Routes = [
-    { path: 'Locations.php', component: HousingLocationComponent },
-    { path: '', redirectTo: '/Locations.php', pathMatch: 'full' }
-];
