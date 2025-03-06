a396d3fffca8850194436998678b16041e72f79b
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-03-05 19:11:26 -0500

    02. Consumo de la APIREST con ANGULAR

--- frontend/src/app/services/housing-location.service.ts
+++ frontend/src/app/services/housing-location.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HousingLocation } from '../interfaces/housing-location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingLocationService {
+  private url = 'http://localhost/api_php_angular/backend/controllers/Locations.php';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllLocations(): Observable<any> {
+    return this.http.get<any>(this.url);
+  }
 }
