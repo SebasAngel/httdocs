1b55cfed2947cdf9ed128a8c65d809bf602c24a2
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-07 18:47:39 -0500

    APIREST en PHP y Angular

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DetailsComponent } from './components/details/details.component';
-import { HomeComponent } from './components/home/home.component';
-
-export const routes: Routes = [
-    { path: 'locations', component: HomeComponent, title: 'Home Page' },
-    { path: '', redirectTo: '/locations', pathMatch: 'full' },
-    { path: 'locations/:id', component: DetailsComponent, title: 'Home Details' }
-];
